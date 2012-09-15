fa76386c7df341226ff3f0303ba3e81671c8a63c
Jeremy Kay <jeremykaybass@gmail.com>
2012-09-15 00:07:25 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jkblog::Application.config.secret_token = 'c5d0ab90253573f70ee2ca69f37157bb48504645e9d1d7c160768307301cbb79216b787948c70513ceecf44e208e96f994ff8b79486f798f15a099d70720efe8'
